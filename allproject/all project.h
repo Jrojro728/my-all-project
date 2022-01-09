24049aee1b426303102c859d27233f7eff109e35
Jrojro728 <64339010+Jrojro728@users.noreply.github.com>
2022-01-09 18:02:33 +0800

    Add files via upload

---
+++ allproject/all project.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+#include <iostream>
+
+/*
+     作者：赵城（苦瓜）
+     时间：2022年1月9日
+
+     此头文件是我所有程序的集合（包括一些我用来的偷懒函数），只要引用此头文件就可以使用我的程序。
+*/
+
+
+int count1(int x, int y)
+{
+    return x * 2 + y * 4;
+}
+
+int 鸡兔同笼(int jiao,int a)
+{
+    int temp;
+    int call;
+    int x{};
+    int y{};
+
+    x = a;
+    for (call = 0; call < 10000; call++)
+    {
+        temp = x * 2 + y * 4;
+        if (jiao == temp)
+        {
+            std::cout << "========结果========\n";
+            std::cout << "鸡的数量是：" << x << "\n" << "兔子的数量是：" << y << "\n" << "运算了" << call << "次" << "\n";
+            break;
+        }
+        else
+        {
+            x = x - 1;
+            y = y + 1;
+        }
+    }
+
+    return 0;
+}
+
+int math1(int x,int y)
+{
+    int temp1;
+    int temp2;
+    temp1 = x - y;
+    temp2 = x + y;
+    std::cout << "这个平行四边形的面积是：" << temp1 * temp2 << "\n";
+    
+    return 0;
+}
